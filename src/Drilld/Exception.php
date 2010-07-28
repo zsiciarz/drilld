dea4467d9d931f4392d3bc0785cd841fd1e69a5c
Zbigniew Siciarz <antyqjon@gmail.com>
2010-07-28 15:32:55 +0200

    Added a simplest possible class just to test the loader.

---
+++ src/Drilld/Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drilld;
+
+/**
+ * This file is part of the Drilld PHP security framework.
+ * Drilld is free software, licensed under the MIT/X11 License. A copy of
+ * the license is provided with the library in the LICENSE file.
+ */
+
+/**
+ * Exception class.
+ */
+class Exception extends \Exception
+{
+}
